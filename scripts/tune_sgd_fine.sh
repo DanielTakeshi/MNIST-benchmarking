0a8d29539f5e71127bbae17d9cb7d96f1455ad71
DanielTakeshi <takeshidanny@gmail.com>
2018-02-23 22:29:47 -0800

    Now running RMS prop tuning script

--- scripts/tune_sgd_fine.sh
+++ scripts/tune_sgd_fine.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-
+size=400
 for e in 0.045 0.055 0.065 0.075; do
     for w in 0.0 0.00001; do
         for (( i=1 ; i<=20 ; i++ )); do
-            python mnist_fc.py --optimizer sgd --fc_size 400 --lrate $e --l2_reg $w --seed $i > logs/sgd-tune/fc-400-lrate-$e-wd-$w-seed-$i
+            python mnist_fc.py --optimizer sgd --fc_size $size --lrate $e --l2_reg $w --seed $i > logs/sgd-tune/fc-$size-lrate-$e-wd-$w-seed-$i
         done
     done
 done
