28df492c17766aff9611f2e11a98ca5a1873aaea
DanielTakeshi <takeshidanny@gmail.com>
2018-02-24 19:50:08 -0800

    Got RMS coarse results

--- README.md
+++ README.md
@@ -3,6 +3,13 @@
 Tuning of hyperparameters. Use the shell scripts and just use file redirecting,
 as in `python stuff.py > file.txt`.
 
-Some RMSProp observations after the coarse stage:
+Some RMSProp observations after the coarse stage, with 55k training, best
+validations:
 
+1. lrate 0.001, wd 0.000001, v=1.275, t=1.402
+2. lrate 0.001, wd 0.00001, v=1.285, t=1.462
+3. lrate 0.005, wd 0.0, v=1.295, t=1.425
+4. lrate 0.001, wd 0.0001, v=1.360, t=1.490
+5. lrate 0.005, wd 0.000001, v=1.390, t=1.535
 
+These are average values over 4 standard deviations. BTW.
--- scripts/tune_rmsprop_fine.sh
+++ scripts/tune_rmsprop_fine.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 size=400
-for e in 0.0003 0.0006 0.0009 0.0012 0.0015 ; do
-    for w in 0.0 0.00001 ; do
+for e in 0.0001 0.0002 0.0003 0.0004 0.0005 ; do
+    for w in 0.000001 0.00001 ; do
         for (( i=1 ; i<=20 ; i++ )) ; do
-            python mnist_fc.py --optimizer rmsprop --fc_size $size --lrate $e --l2_reg $w --seed $i > logs/rmsprop-tune/fc-$size-lrate-$e-wd-$w-seed-$i
+            python mnist_fc.py --optimizer rmsprop --fc_size $size --lrate $e --l2_reg $w --seed $i > logs/rmsprop-fine-tune/fc-$size-lrate-$e-wd-$w-seed-$i
         done
     done
 done
