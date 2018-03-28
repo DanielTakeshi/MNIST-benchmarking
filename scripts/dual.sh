452b1f58caab031664760e0bd04a484cd02a0257
DanielTakeshi <takeshidanny@gmail.com>
2018-03-28 09:43:12 -0700

    Now running 1700 on my computer. Also ...
    always pull before switching to new computers ...

--- scripts/dual.sh
+++ scripts/dual.sh
@@ -1,3 +1,3 @@
 for ((b=0; b<3; b++)); do
-    python targ_digit_test.py --num_train 2000 --num_epochs 3000 --seed $b --batch_size 200 --lrate 0.001  --cnn_arch 2
+    python targ_digit_test.py --scale_output --num_train 1700 --num_epochs 5000 --seed $b --batch_size 200 --lrate 0.001  --cnn_arch 2
 done
